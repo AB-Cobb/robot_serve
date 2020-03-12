41cc32597e168890d7acaff1d5a0de4d0d1482d2
Andrew Cobb <andrew@cobbs-web.com>
2020-03-12 13:20:20 -0400

    logging from contrl

--- public/ctrl.js
+++ public/ctrl.js
@@ -3,14 +3,17 @@ $(function(){
     const socket = io.connect(/*'localhost:3000'*/)
     
     // Input / output
-    var pi_logs = $("#logs")
+    var logs = $("#logs")
     var btn_fwd = $("#FWD")
     var btn_lft = $("#LFT")
     var btn_bck = $("#BCK")
     var btn_rght = $("#RGHT")
 
     socket.on ("PI_log", data => {
-        pi_logs.append("<p>" +data.txt+"</p>")
+        logs.append("<p>PI: " +data.txt+"</p>")
+    })
+    socket.on ("cmd_log", data => {
+        logs.append("<p>CMD:" +data.txt+"</p>")
     })
 
     btn_fwd.mousedown(function(){
