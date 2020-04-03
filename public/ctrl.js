65ab010504207b5e34c339cdf52231fbdb04477d
Andrew Cobb <andrew@cobbs-web.com>
2020-04-03 19:21:25 -0400

    front end cam

--- public/ctrl.js
+++ public/ctrl.js
@@ -1,7 +1,6 @@
 $(function(){
     console.log("hello world")
     const socket = io.connect(/*'localhost:3000'*/)
-    const stream = ss.createStream();
     // Input / output
     var logs = $("#logs")
     var btn_fwd = $("#FWD")
